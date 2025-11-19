e856e908c37a559ccf044a1f8b8b55138e03bd82
Renan Greca <renan.greca@canonical.com>
2025-11-19 12:49:03 +0100

    changed github actions

--- .github/scripts/setup-artifact-import.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-echo "Starting local registry so plan-integration can push images..."
-docker run -d -p 32000:5000 --restart=always --name registry registry:2 || true
